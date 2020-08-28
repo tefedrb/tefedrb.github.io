739864fb909de914947a25ed392bed0f6cb9cb9a
tefedrb-enterprise <tefebell@gmail.com>
2020-08-28 17:20:49 -0400

    p5 library active and set up in tree - canvas shows up

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -4,6 +4,7 @@ import p5 from 'p5';
 const Tree = (props) => {
     const [ data, updateDate ] = useState({});
     const treeContainer = useRef();
+    let myP5;
 
     const s = (sketch) => {
         let x = 100;
@@ -16,13 +17,13 @@ const Tree = (props) => {
         sketch.draw = () => {
             sketch.background(0);
             sketch.fill(255);
-            sketch.rect(x,y,50,50);
+            sketch.rect(x,y,50,75);
         };
     }
 
     useEffect(() => {
-        treeContainer.current = new p5(s);
-    }, [])
+       myP5 = new p5(s, treeContainer.current);
+    }, [myP5])
 
     return (
         <>
