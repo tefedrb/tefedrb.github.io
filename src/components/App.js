02fb10007a78f59ea0669fc064ff384a143187d8
tefedrb-enterprise <tefebell@gmail.com>
2020-10-18 09:45:00 -0400

    testing new Router attribute

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,26 @@ import { hot } from 'react-hot-loader';
 import Home from './Home';
 import { Provider } from '../context';
 import Monitor from './Monitor/Monitor';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 const App = () => (
-    <Router>
-        <Provider>
-            <Monitor>
-                <Home />
-            </Monitor> 
-        </Provider>
+    <Router basename={process.env.PUBLIC_URL}>
+        {console.log("testing 4")}
+        {console.log(process.env.PUBLIC_URL, " <-- public url")}
+        <Switch>
+            
+            <Route path="/test">
+                <div>Testing</div>
+            </Route>
+            <Route path="/">
+                <Provider>
+                    <Monitor>
+                        <Home />
+                    </Monitor> 
+                </Provider>
+            </Route>
+        </Switch>
     </Router>
 );
 
