09bdf71eb020912c9bb813571739d1847b759fae
tefedrb-enterprise <tefebell@gmail.com>
2020-11-24 12:25:43 -0500

    adjusting webpack configuration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './css/styles.css'
+// import './css/crt.scss'
 
 ReactDOM.render(
     <App />,
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,62 @@
 const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+// Default minimizer for JavaScript & Webpack
+const TerserPlugin = require("terser-webpack-plugin");
+
+////////////////////////////////////////////////////////////////
+
+
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const HtmlWebPackPluginConfig = new HtmlWebPackPlugin({
+    template: './src/index.html',
+    filename: './index.html',
+    minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        removeComments: true
+    }
+});
+////////////////////////////////////////////////////////////////
 
 module.exports = merge(common, {
     mode: "production",
     output: {
-        filename: "main.[contentHash].js",
+        filename: "[name].[contentHash].js",
         path: path.resolve(__dirname, "dist")
     },
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin(),
+            new TerserPlugin(),
+            HtmlWebPackPluginConfig
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: "[name].[contentHash].css"}),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    }
 });
